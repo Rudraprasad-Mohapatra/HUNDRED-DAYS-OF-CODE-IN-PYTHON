362874763ca3ac5c3b279d0f4dd7dfc51251cc6d
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2022-12-24 00:17:48 +0530

    Day-7's Code

---
+++ CLCULATOR_USING_PYTHON_SOLUTION7.py
@@ -0,0 +1,12 @@
+# Calculator Solution
+a=50
+b=3
+print("Addition of",a,"+",b,"is",a+b)
+print("Multiplication of",a,"*",b,"is",a*b)
+print("Subtraction of",a,"-",b,"is",a-b)
+print("Division of",a,"/",b,"is",a/b) #Division of 50 and 3 is 16.666666666666668
+print("Modulus of",a,"%",b,"is",a%b)
+# Exponential Operator
+print("Exponentiation of",a,"**",b,"is",a**b) # 5**3=5*5*5,5**2=5*5
+# Floor Division will give the Quotient Value
+print("Floor Division of",a,"//",b,"is",a//b)
