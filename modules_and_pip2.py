9f1efb7f6798f6411e38da4d346437963d0d1215
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2022-12-23 11:40:16 +0530

    Day-2's code

---
+++ modules_and_pip2.py
@@ -0,0 +1,4 @@
+# import hashlib
+import pandas #As we have prinstalled pandas in command prompt ,this shows no error
+# import tensorflow # it shows error as we have not installed it through command prompt,this shows error
+print("HI")
