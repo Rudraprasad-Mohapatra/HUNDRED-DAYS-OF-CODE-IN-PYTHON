a20dbbdf8baafea3298516bcc49b11d84d338bd7
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2022-12-24 01:15:16 +0530

    Day-10's code

---
+++ AGE_Calculation_in_PYTHON10.py
@@ -0,0 +1,5 @@
+
+a=input("Enter your name :- ")
+Byear=input("Enter your Birth Year :- ")
+Oyear=input("Enter the current ongoing year :- ")
+print("Your name is",a,"and your age is :- ",((int(Oyear)-int(Byear))+1))
