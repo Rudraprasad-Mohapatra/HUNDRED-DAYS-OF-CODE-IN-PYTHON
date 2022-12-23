864b5e32eeaef73ae0add28fb74b01c740895f4f
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2022-12-23 23:59:32 +0530

    day-6's code

---
+++ CALCULATOR_using_python6.py
@@ -0,0 +1,9 @@
+# OPERATORS
+print("Addition of 5 and 3 is " ,5+3)
+print("Multiplication of 5 and 3 is " ,5*3)
+print("Subtraction of 5 and 3 is " ,5-3)
+print("Division of 5 and 3 is " ,5/3)
+print("Modulus of 5 and 3 is " ,5%3)
+# Exponential Operator
+print("Exponentiation of 5 and 3 is " ,5**3) # 5**3=5*5*5,5**2=5*5
+print("Floor Division of 5 and 3 is " ,5//3)
