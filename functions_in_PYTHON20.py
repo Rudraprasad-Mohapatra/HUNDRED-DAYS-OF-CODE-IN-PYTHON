f6ab0d2c34736ceb3dbf4b93136b1729c2e7f696
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2022-12-25 21:32:10 +0530

    Day-20's code

---
+++ functions_in_PYTHON20.py
@@ -0,0 +1,20 @@
+def calculateGmean(a1,b1):
+    gmean=(a1*b1)/(a1+b1)
+    print(gmean)
+
+def checking_Num(a,b):
+    if(a>b):
+        print(a,"is greater than",b)
+    else:
+        print(b,"is greater than",a)  
+
+# def addTwoNumWithInput(a,b):  #Writing only this sigle line without dfinition will give error
+# But let we want now interpreter should not give error and move forward and I will define it latter then we have to use "pass", So code will be:-
+def addTwoNumWithInput(a,b):
+    pass
+
+    
+a=9
+b=8
+calculateGmean(a,b)
+checking_Num(a,b)
