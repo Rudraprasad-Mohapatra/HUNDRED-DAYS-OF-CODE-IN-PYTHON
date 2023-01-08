7ce21e19a9b02b7e593b38d14d6cd11ba633b7d1
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-01-08 23:23:08 +0530

    Day-42's Code

---
+++ Enumerate_Function_In_PYTHON42.py
@@ -0,0 +1,18 @@
+marks=[12,56,32,98,12,45,1,4]
+
+# concept-1
+# index=0
+# for mark in marks:
+#     print(mark)
+#     if(index==3):
+#         print("Harry,Awesome")
+#     index+=1    
+
+# enumerate()
+#  Enumerate() is an built in function which allows loop all over a sequence like list,tuple or string and get the index and elementValue at same time 
+# marks=(2,5,6)
+for index,mark in enumerate(marks,start=1):
+    print(index,mark,type (marks))
+    if(index==3):
+        print("Harry,awesome!")
+
