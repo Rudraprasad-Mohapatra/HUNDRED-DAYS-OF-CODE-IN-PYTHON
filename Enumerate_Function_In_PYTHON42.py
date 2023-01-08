5d4050893e83b6676402f69bb8857573cc5db1ea
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-01-08 23:23:41 +0530

    Day-42's Code

--- Enumerate_Function_In_PYTHON42.py
+++ Enumerate_Function_In_PYTHON42.py
@@ -11,7 +11,8 @@ marks=[12,56,32,98,12,45,1,4]
 # enumerate()
 #  Enumerate() is an built in function which allows loop all over a sequence like list,tuple or string and get the index and elementValue at same time 
 # marks=(2,5,6)
-for index,mark in enumerate(marks,start=1):
+# for index,mark in enumerate(marks,start=1):
+for index,mark in enumerate(marks,start=1): #By mentioning start=1 index will start from '1' instead of '0' i.e. 0th index element will be 1st index element.Index will be start from 1 no element value will change.
     print(index,mark,type (marks))
     if(index==3):
         print("Harry,awesome!")
