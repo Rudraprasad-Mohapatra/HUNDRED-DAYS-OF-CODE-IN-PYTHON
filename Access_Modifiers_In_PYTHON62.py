cb8a0337356f4d3a5d231a8faa20ebf0e9974995
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2023-01-30 00:19:02 +0530

    Day-62's Code (Rearranged Notes)

--- Access_Modifiers_In_PYTHON62.py
+++ Access_Modifiers_In_PYTHON62.py
@@ -16,8 +16,25 @@ print(dir(a))
 """
 NOTES:-
 
+
 Access Specifiers/Modifiers
 Access specifiers or access modifiers in python programming are used to limit the access of class variables and class methods outside of class while implementing the concepts of inheritance.
+
+# Name mangling
+
+Name mangling in Python is a technique used to protect class-private and superclass-private attributes from being accidentally overwritten by subclasses. Names of class-private and superclass-private attributes are transformed by the addition of a single leading underscore and a double leading underscore respectively.
+
+class MyClass:
+    def __init__(self):
+        self._nonmangled_attribute = "I am a nonmangled attribute"
+        self.__mangled_attribute = "I am a mangled attribute"
+
+my_object = MyClass()
+
+print(my_object._nonmangled_attribute) # Output: I am a nonmangled attribute
+print(my_object.__mangled_attribute) # Throws an AttributeError
+print(my_object._MyClass__mangled_attribute) # Output: I am a mangled attribute
+In the example above, the attribute _nonmangled_attribute is marked as nonmangled by convention, but can still be accessed from outside the class. The attribute __mangled_attribute is private and its name is "mangled" to _MyClass__mangled_attribute, so it can't be accessed directly from outside the class, but you can access it by calling _MyClass__mangled_attribute
 
 Let us see the each one of access specifiers in detail:
 
@@ -80,21 +97,6 @@ AttributeError: 'student' object has no method '__funName()'
 AttributeError: 'subject' object has no attribute '__age'
 AttributeError: 'student' object has no method '__funName()'
 Private members of a class cannot be accessed or inherited outside of class. If we try to access or to inherit the properties of private members to child class (derived class). Then it will show the error.
-
-Name mangling
-Name mangling in Python is a technique used to protect class-private and superclass-private attributes from being accidentally overwritten by subclasses. Names of class-private and superclass-private attributes are transformed by the addition of a single leading underscore and a double leading underscore respectively.
-
-class MyClass:
-    def __init__(self):
-        self._nonmangled_attribute = "I am a nonmangled attribute"
-        self.__mangled_attribute = "I am a mangled attribute"
-
-my_object = MyClass()
-
-print(my_object._nonmangled_attribute) # Output: I am a nonmangled attribute
-print(my_object.__mangled_attribute) # Throws an AttributeError
-print(my_object._MyClass__mangled_attribute) # Output: I am a mangled attribute
-In the example above, the attribute _nonmangled_attribute is marked as nonmangled by convention, but can still be accessed from outside the class. The attribute __mangled_attribute is private and its name is "mangled" to _MyClass__mangled_attribute, so it can't be accessed directly from outside the class, but you can access it by calling _MyClass__mangled_attribute
 
 # Protected Access Modifier
 
